9b7b876b2a036397683ecfe7ace2fbe50b265a87
arnavs06 <arnavsk1006@gmail.com>
2025-03-07 17:53:54 -0500

    trying to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend', // Ensure this points to the correct location
+  root: '.', // Keep this as '.' since Vercel now starts in 'frontend/'
   build: {
     outDir: 'dist', // Ensure Vercel expects 'dist'
+    rollupOptions: {
+      input: 'index.html', // Explicitly tell Vite to use this file
+    }
   }
 });
