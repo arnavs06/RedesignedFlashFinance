3e3c7a11db3339c0ee2c877ce90911cd1c86c9cf
arnavs06 <arnavsk1006@gmail.com>
2025-03-12 19:46:36 -0400

    work now plz routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Flashcard from './pages/Flashcards';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import './styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Flashcard from "./pages/Flashcards";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import "./styles/App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/flashcards" element={<Flashcard flashcard={Flashcard} />} />
+          <Route path="/flashcards" element={<Flashcard />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
