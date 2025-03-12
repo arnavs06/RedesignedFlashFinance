d088399b33a45491c2a5fdc51360c10b9c4d6d53
arnavs06 <arnavsk1006@gmail.com>
2025-03-12 19:54:22 -0400

    could be it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensures correct routing on Vercel
   root: '.', // Keep this as '.' since Vercel now starts in 'frontend/'
   build: {
     outDir: 'dist', // Ensure Vercel expects 'dist'
