43bd70c885ca174d4881a25c60cf474c0d9dc969
arnavs06 <arnavsk1006@gmail.com>
2025-03-19 23:36:12 -0400

    work plz

--- frontend/src/components/Landing/Testimonials.jsx
+++ frontend/src/components/Landing/Testimonials.jsx
@@ -4,12 +4,12 @@ import { motion } from 'framer-motion';
 const Testimonials = () => {
   const testimonials = [
     {
-      quote: Arnav,
-      author: "Hi, I'm Arnav, the lead developer behind Flash Finance.",
+      quote: "Hi, I'm Arnav, the lead developer behind Flash Finance. knowing the struggle of not being a Finance/Acounting Major, I had sought out to create a product which I would personally use and learn from, and hope to leverage this application in my own recruitment process. If anyone has any questions or anything, feel free to contact us!",
+      author: "Arnav, Co-Founder / Lead Developer",
     },
     {
-      quote: "Dennis",
-      author: "To be done",
+      quote: "We founded Flash Finance to be by students for students! I personally plan on using Flash Finance to help me review for investment banking interviews and  I hope it helps others too. If you guys have any suggestions please contact us.",
+      author: "Dennis, Co-Founder, Head of Product Development",
     },
   ];
 
